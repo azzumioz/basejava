73ed0ac1e77b9b8640882b8f1bcbd7193378f72a
azzumioz <my@localhost>
2019-01-08 16:59:28 +0700

    HW01 optimize rev2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int size = size();
+    private int size = 0;
 
     void clear() {
         for (int i = 0; i < size; i++) {
@@ -21,34 +21,32 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) return storage[i];
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
         }
-        return null ;
+        return null;
     }
 
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size-1];
-                storage[size-1] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
                 break;
             }
         }
-        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0,size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) count++;
-        }
-        return count;
+        return size;
     }
 }
