6267de106c56f2663bc13ff5591fe56546773991
azzumioz <my@localhost>
2019-01-08 14:02:29 +0700

    HW01 optimize metod

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,58 +1,47 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = size();
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        Resume r = null;
-        for (int i = 0; i < size (); i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                r = storage[i];
-                break;
-            }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
         }
-        return r ;
+        return null ;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
+                storage[i] = storage[size-1];
+                storage[size-1] = null;
                 break;
             }
         }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume [] resumeNotNull = new Resume[size()];
-        int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            try {
-                if (storage[i].uuid != null) resumeNotNull[count++] = storage[i];
-            } catch (NullPointerException e) {
-                //NOP
-            }
-        }
-        return resumeNotNull;
+        return Arrays.copyOfRange(storage, 0,size);
     }
 
     int size() {
