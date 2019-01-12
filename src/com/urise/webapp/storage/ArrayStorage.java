2d30daed97e5d459be6613ca6a5b87b096325741
azzumioz <my@localhost>
2019-01-12 22:48:13 +0700

    Add static final STORAGE_LIMIT

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
+    private static final int STORAGE_LIMIT = 10_000;
+
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -17,7 +19,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        if (size < storage.length) {
+        if (size < STORAGE_LIMIT) {
             if (getIndex(r.getUuid()) == -1) {
                 storage[size] = r;
                 size++;
