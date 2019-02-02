764ed6184777acd510105bfe0bc031d0f0db1acd
azzumioz <my@localhost>
2019-02-02 16:49:02 +0700

    HW06 add MapStorage class

--- src/com/urise/webapp/storage/MapResumeStorage.java
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -2,10 +2,7 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.*;
-
-public class MapResumeStorage extends AbstractStorage {
-    protected Map<String, Resume> storage = new HashMap<>();
+public class MapResumeStorage extends MapStorage {
 
     @Override
     public void save(Resume r) {
@@ -20,11 +17,6 @@ public class MapResumeStorage extends AbstractStorage {
     }
 
     @Override
-    public void clear() {
-        storage.clear();
-    }
-
-    @Override
     protected void doSave(Object searchKey, Resume r) {
         storage.put(r.getFullName(), r);
     }
@@ -33,44 +25,5 @@ public class MapResumeStorage extends AbstractStorage {
     protected void doUpdate(Object searchKey, Resume r) {
         storage.replace(r.getFullName(), r);
     }
-
-    @Override
-    protected Resume doGet(Object searchKey) {
-        return storage.get(searchKey);
-    }
-
-
-    @Override
-    public Resume[] getAll() {
-        return storage.values().toArray(new Resume[0]);
-    }
-
-    @Override
-    public List<Resume> getAllSorted() {
-        List<Resume> sortedList = new ArrayList(storage.values());
-        Collections.sort(sortedList, RESUME_COMPARATOR_FULL_NAME);
-        return sortedList;
-    }
-
-    @Override
-    protected void doDelete(Object searchKey) {
-        storage.remove(searchKey);
-    }
-
-    @Override
-    public int size() {
-        return storage.size();
-    }
-
-    @Override
-    protected Object getSearchKey(String fullName) {
-        return fullName;
-    }
-
-    @Override
-    protected boolean isExist(Object searchKey) {
-        return storage.containsKey(searchKey);
-    }
-
 
 }
